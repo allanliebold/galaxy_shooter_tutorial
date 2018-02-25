8c0454c580397ec52500b9e8efb82551d6cc3506
Allan Liebold <allan.liebold@gmail.com>
2018-02-24 23:52:25 -0800

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour {
   //public or private identify
   //data type - int, floats, bool, strings
   
+  public float speed;
+  
   void Start () {
     //Debug.Log(transform.position);
     transform.position = new Vector3(0, 0, 0);
