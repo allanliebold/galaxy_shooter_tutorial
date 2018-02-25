03d621e3fee622480d022d3d83e97b2ae3480bec
Allan Liebold <allan.liebold@gmail.com>
2018-02-24 23:50:58 -0800

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour {
   }
   
   void Update () {
-    transform.Translate(new Vector3.right * Time.deltaTime);
+    transform.Translate(new Vector3.right * Time.deltaTime * 5);
   }
 
 }
