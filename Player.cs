76158d261798bee723439c03e8b86d5ac758b70c
Allan Liebold <allan.liebold@gmail.com>
2018-02-24 23:35:07 -0800

    Create Player.cs

---
+++ Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngin;
+
+public class Player : MonoBehaviour {
+  void Start () {
+  
+  }
+  
+  void Update () {
+  
+  }
+
+}
