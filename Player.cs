e7d8c1cb03761f4bba63be15b3d84f92709f3b09
Allan Liebold <allan.liebold@gmail.com>
2018-02-25 19:48:47 -0800

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("x pos: " + transform.position.x);
+		Debug.Log (transform.position);
 	}
 	
 	// Update is called once per frame
