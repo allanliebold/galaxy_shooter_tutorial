8cc028559cada9ee807334f95ed9996e48899e88
Allan Liebold <allan.liebold@gmail.com>
2018-02-25 19:38:34 -0800

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -3,19 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-  //public or private identify
-  //data type - int, floats, bool, strings
-  
-  public float speed = 5.0f;
-  
-  void Start () {
-    //Debug.Log(transform.position);
-    transform.position = new Vector3(0, 0, 0);
-  }
-  
-  void Update () {
-    transform.Translate(new Vector3.right * Time.deltaTime * 20);
-    speed = 10;
-  }
 
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
