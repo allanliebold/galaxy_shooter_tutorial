f9a18cbbfc55c33f4608a8f46cfcdbdbc35b9c52
Allan Liebold <allan.liebold@gmail.com>
2018-02-26 06:08:39 -0800

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -14,6 +14,6 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.right * Time.deltaTime * speed);
+		transform.Translate (Vector3.left * Time.deltaTime * speed);
 	}
 }
