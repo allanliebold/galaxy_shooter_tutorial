86f47928aecad0eb5dfba70287a475ef7427d451
Allan Liebold <allan.liebold@gmail.com>
2018-02-28 06:00:40 -0800

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -13,6 +13,10 @@ public class Player : MonoBehaviour {
 	}
 	
 	private void Update () {
+		Movement();
+	}
+	
+	private void Movement () {
 		float horizontalInput = Input.GetAxis ("Horizontal");
 		float verticalInput = Input.GetAxis ("Vertical");
 		
