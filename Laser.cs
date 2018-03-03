510ac129157f2a538e3c4e93c05e059441dfdcef
Allan Liebold <allan.liebold@gmail.com>
2018-03-03 12:02:32 -0800

    Update Laser.cs

--- Laser.cs
+++ Laser.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Laser : MonoBehaviour {
-	public float speed = 10.0f;
+	public float _speed = 10.0f;
 
 	void Update () {
-		transform.Translate(Vector3.up * speed * Time.deltaTime);
+		transform.Translate(Vector3.up * _speed * Time.deltaTime);
 		
 		if(transform.position.y >= 6) {
 			Destroy(this.gameObject);
