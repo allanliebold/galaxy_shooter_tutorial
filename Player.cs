ab5ef4699ded0525a19407607b78a2481d9353f9
Allan Liebold <allan.liebold@gmail.com>
2018-03-03 23:07:25 -0800

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -30,6 +30,7 @@ public class Player : MonoBehaviour {
 	// Implement triple shot power up 
 		
 		Instantiate(_laserPrefab, transform.position + new Vector3(0.55f, 0.06f, 0), Quaternion.identity);
+		Instantiate(_laserPrefab, transform.position + new Vector3(-0.55f, 0.06f, 0), Quaternion.identity);
 		
 		if(Time.time > _canFire) {
 			Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
