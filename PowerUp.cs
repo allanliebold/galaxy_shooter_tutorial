e73d38f64ce2e153f3b0c4d1df9a674a2eb1bfdb
Allan Liebold <allan.liebold@gmail.com>
2018-03-04 21:08:07 -0800

    Update PowerUp.cs

--- PowerUp.cs
+++ PowerUp.cs
@@ -10,7 +10,7 @@ public class PowerUp : MonoBehaviour {
 	}
 	
 	void Update () {
-		transform.Translate (Vector3.down * Time.deltaTime);	
+		transform.Translate (Vector3.down * _speed * Time.deltaTime);	
 	}
 }
 
