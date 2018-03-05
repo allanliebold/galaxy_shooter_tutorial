b59a9283507ff2c86fdd4f022977832e5d55350b
Allan Liebold <allan.liebold@gmail.com>
2018-03-04 21:17:27 -0800

    Update PowerUp.cs

--- PowerUp.cs
+++ PowerUp.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour {
 	private float _speed = 3.0f;
-
-	void Start () {
-		
-	}
 	
 	void Update () {
 		transform.Translate (Vector3.down * _speed * Time.deltaTime);	
