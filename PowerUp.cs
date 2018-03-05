b8695d1cd13b3a5801f46fc74987fae355ec7df4
Allan Liebold <allan.liebold@gmail.com>
2018-03-04 21:29:45 -0800

    Update PowerUp.cs

--- PowerUp.cs
+++ PowerUp.cs
@@ -11,6 +11,9 @@ public class PowerUp : MonoBehaviour {
 	
 	private void OnTriggerEnter2D (Collider2D other) {
 		Debug.Log("Collided with " + other.name);
+		// Access Player
+		// Set TripleShot bool to true
+		// Destroy self
 	}
 }
 
