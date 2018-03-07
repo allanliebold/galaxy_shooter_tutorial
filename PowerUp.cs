98365e33146744d03eea3b0ea254cabeb488b825
Allan Liebold <allan.liebold@gmail.com>
2018-03-07 06:03:06 -0800

    Update PowerUp.cs

--- PowerUp.cs
+++ PowerUp.cs
@@ -10,7 +10,7 @@ public class PowerUp : MonoBehaviour {
 	}
 	
 	private void OnTriggerEnter2D (Collider2D other) {
-		if (other.tag == "Player") {
+		if (this.tag = "TripleShot" and other.tag == "Player") {
 			Player player = other.GetComponent<Player>();
 			if (player != null) {
 				player.TripleShotOn();
