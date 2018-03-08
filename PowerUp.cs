6cedbdb0f326872e23c4f08d777fc2bb39bb7487
Allan Liebold <allan.liebold@gmail.com>
2018-03-07 17:39:34 -0800

    Update PowerUp.cs

--- PowerUp.cs
+++ PowerUp.cs
@@ -6,7 +6,7 @@ public class PowerUp : MonoBehaviour {
 	[SerializedField]
 	private float _speed = 3.0f;
 	[SerializedField]
-	private int powerUpID; // 0 = Triple Shot, 1 = Speed Boost
+	private int powerUpID; // 0 = Triple Shot, 1 = Speed Boost, 2 = Shield
 	
 	void Update () {
 		transform.Translate (Vector3.down * _speed * Time.deltaTime);	
