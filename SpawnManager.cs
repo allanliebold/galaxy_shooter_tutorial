05881bf227ce330e8ea2437a2f63267340d7d3d6
Allan Liebold <allan.liebold@gmail.com>
2018-03-15 06:02:19 -0700

    Update SpawnManager.cs

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -3,26 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour {
-	
-	private float spawnTimer = 4.0f;
-	private float nextSpawn;
-	private int killCount = 0;
-	
-	[SerializedField]
-	private GameObject _enemy;
-	[SerializedField]
-	private GameObject _powerUp;
-	
-	void Update () {
-		
-	}
-	
-	void SpawnEnemy () {
-		// Instantiate new enemy
-	}
-	
-	void SpawnPowerUp () {
-		// Instantiate power up. Timer or based on number of enemies destroyed?
-		// If killCount = 10 spawn power up and reset killCount to 0. Actually, this should be on the enemy.
-	}
+	[SerializeField]
+	private GameObject enemyShipPrefab;
 }
