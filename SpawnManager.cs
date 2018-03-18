be56fe78144d6f30a9657f9e03134238e267acf1
Allan Liebold <allan.liebold@gmail.com>
2018-03-17 23:48:46 -0700

    Update SpawnManager.cs

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -14,7 +14,7 @@ public class SpawnManager : MonoBehaviour {
 	}
 	
 	IEnumerator EnemySpawnRoutine() {
-		while (true) {
+		while(true) {
 			Instantiate(enemyShipPrefab, new Vector3(Random.Range(-7f, 7f), 7, 0), Quaternion.identity);	
 			yield return new WaitForSeconds(5.0f);
 		}
